b65732d776a6c6815c871ae2d1fd1ec3310c8f49
Orange <425014465@qq.com>
2019-04-14 23:18:38 +0800

    update

--- app/src/main/java/com/orange/algorithm/v100.java
+++ app/src/main/java/com/orange/algorithm/v100.java
@@ -167,4 +167,17 @@ public class v100 {
         }
         return digits;
     }
+
+    //83. 删除排序链表中的重复元素
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head==null || head.next==null){
+            return head;
+        }
+        head.next=deleteDuplicates(head.next);
+        if (head.val==head.next.val){
+            head=head.next;
+        }
+        return head;
+    }
 }
--- app/src/main/java/com/orange/algorithm/v200.java
+++ app/src/main/java/com/orange/algorithm/v200.java
@@ -4,4 +4,40 @@ package com.orange.algorithm;
  * created by czh on 2019/3/10
  */
 public class v200 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private class TreeResult{
+        int depth;
+        boolean isBTree;
+        public TreeResult(int depth,boolean isBTree){
+            this.depth=depth;
+            this.isBTree=isBTree;
+        }
+    }
+
+    //110. 平衡二叉树
+    public boolean isBalanced(TreeNode root) {
+        return isBTree(root).isBTree;
+    }
+
+    private TreeResult isBTree(TreeNode node){
+        if (node==null){
+            return new TreeResult(0,true);
+        }
+        TreeResult left=isBTree(node.left);
+        TreeResult right=isBTree(node.right);
+        if (!left.isBTree || !right.isBTree || Math.abs(left.depth-right.depth)>1){
+            return new TreeResult(0,false);
+        }
+        return new TreeResult(Math.max(left.depth,right.depth)+1,true);
+    }
 }
