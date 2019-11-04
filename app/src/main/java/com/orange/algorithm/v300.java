c9820ef31fcaf4049faa20bcf12613cf8e56cd4d
OrangeHao <425014465@qq.com>
2019-11-04 17:56:46 +0800

    update

--- app/src/main/java/com/orange/algorithm/v300.java
+++ app/src/main/java/com/orange/algorithm/v300.java
@@ -146,11 +146,11 @@ public class v300 {
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
     public boolean isIsomorphic(String s, String t) {
-        char[] ch1=s.toCharArray();
-        char[] ch2=t.toCharArray();
-        int len=ch1.length;
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+        int len = ch1.length;
         for (int i = 0; i < len; i++) {
-            if (s.indexOf(ch1[i])!=t.indexOf(ch2[i])){
+            if (s.indexOf(ch1[i]) != t.indexOf(ch2[i])) {
                 return false;
             }
         }
@@ -158,21 +158,19 @@ public class v300 {
     }
 
     public boolean isIsomorphic2(String s, String t) {
-        char[] temp=new char[127];
-        char[] temp1=new char[127];
-        char[] S=s.toCharArray();
-        char[] T=t.toCharArray();
-        int length=s.length();
-        for(int i=0;i<length;i++)
-        {
-            if(temp[S[i]]!='\0'||temp1[T[i]]!='\0')
-            {
-                if(temp[S[i]]!=T[i]) {
+        char[] temp = new char[127];
+        char[] temp1 = new char[127];
+        char[] S = s.toCharArray();
+        char[] T = t.toCharArray();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            if (temp[S[i]] != '\0' || temp1[T[i]] != '\0') {
+                if (temp[S[i]] != T[i]) {
                     return false;
                 }
-            }else{
-                temp[S[i]]=T[i];
-                temp1[T[i]]=S[i];
+            } else {
+                temp[S[i]] = T[i];
+                temp1[T[i]] = S[i];
             }
         }
         return true;
@@ -181,9 +179,9 @@ public class v300 {
 
     //217. 存在重复元素      给定一个整数数组，判断是否存在重复元素。
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set= new HashSet<>();
-        for (int temp:nums){
-            if (set.contains(temp)){
+        Set<Integer> set = new HashSet<>();
+        for (int temp : nums) {
+            if (set.contains(temp)) {
                 return true;
             }
             set.add(temp);
@@ -193,8 +191,8 @@ public class v300 {
 
     public boolean containsDuplicate2(int[] nums) {
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length-1; i++) {
-            if (nums[i]==nums[i+1]){
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
         }
@@ -205,14 +203,14 @@ public class v300 {
     //给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k。
 
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer,Integer>map=new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])){
+            if (map.containsKey(nums[i])) {
                 return true;
             }
-            map.put(nums[i],i);
-            if (map.size()>k){
-                map.remove(nums[i-k]);
+            map.put(nums[i], i);
+            if (map.size() > k) {
+                map.remove(nums[i - k]);
             }
         }
         return false;
@@ -232,4 +230,81 @@ public class v300 {
         return false;
     }
 
+
+    //258. 各位相加
+
+    /**
+     * 给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。
+     * <p>
+     * 示例:
+     * 输入: 38
+     * 输出: 2
+     * 解释: 各位相加的过程为：3 + 8 = 11, 1 + 1 = 2。 由于 2 是一位数，所以返回 2。
+     */
+    public int addDigits(int num) {
+        while (num > 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum = sum + num % 10;
+                num = num / 10;
+            }
+            System.out.println(sum);
+            num = sum;
+        }
+        return num;
+    }
+
+    /**
+     * 时间复杂度为O(1)的解法：
+     * <p>
+     * 这个题目的O(1)解法是对9取余数，我们可以通过归纳总结得到，但是无法直观想清楚原理，或者推导公式，这里我用最直观的画图和推导来讲清楚原理。假设有N个石头，我们按照十进制给这N个石头聚堆。
+     * 按照十进制聚堆的意思是：1个为1堆，10个为1堆，100个为1堆，也就是10的m次方能聚成1堆
+     * 假如石头有123个，那么能聚成几堆？
+     * <p>
+     * 如上图所示，能聚成6堆，其实这就是题目最直观的解法。有k1个(100)堆，百位数就是k1；有k2个(10)堆，十位数就是k2；有k3个(1)堆，个位数就是k3。
+     * 堆的个数就是题目要求的各个位上的数之和。设求堆个数的函数为F(N),那么有：
+     * F(123) = (100 - 99) + (10 - 9) + (10 - 9) + 1 + 1 + 1 = 123 - 99x1 -9x2 = 123 - 9x13
+     * 也就是每个类别的堆，去掉9或者99，就能得到堆个数，假如是(1000)堆，去掉999就行了，然后把所有加起来，得到结果，那么我们知道有:
+     * F(N) = N - 9xM1
+     * 那么问题来了，假如(N - 9xM1)大于10，是不满足要求的，但(N - 9xM1)也是一个数，我们继续可以调用F(N)这个函数：
+     * F(N - 9xM) = N - 9xM1 - 9xM2
+     * 按照题目设定，只要是大于10，就继续在得到的结果上操作，则有:
+     * F(N) = F(N - 9xM) = N - 9xM1 -9xM2 = ...
+     * 依次类推，直到满足条件:
+     * F(N) = N - 9xM,其中M = (M1+M2+M3+...)；并且结果小于10且大于0，因为堆的个数不可能为0个，这就是对9求余数的的算式，但是稍有变化
+     * 当 (N % 9) != 0:
+     * F(N) = N % 9
+     * 当(N % 9) == 0时，N的结果和9是等价的，那么应该是:
+     * F(N) = F(N - 9xM) = F(9) = 9
+     * 不能是F(N) = F(0)，这样就错了
+     * 推导不完全严谨，大家理解意思就行
+     * <p>
+     * 作者：zui-qiang-coder
+     * 链接：https://leetcode-cn.com/problems/add-digits/solution/python-hua-tu-jiang-ming-bai-o1zuo-fa-de-yuan-li-b/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+
+    //263. 丑数
+    //丑数就是只包含质因数 2, 3, 5 的正整数。
+    public boolean isUgly(int num) {
+        while (true){
+            int temp=num;
+            if (num%2==0){
+                num=num/2;
+            }
+            if ((num%3==0)){
+                num=num/3;
+            }
+            if (num%5==0){
+                num=num/5;
+            }
+            if (num==1){
+                return true;
+            }
+            if (num==temp){
+                return false;
+            }
+        }
+    }
 }
